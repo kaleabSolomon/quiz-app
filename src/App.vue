7cb9e35a3494a15f704aa9a5cb510fd88c0e8712
Kaleab Solomon <kaleabslmn@gmail.com>
2023-08-01 16:34:10 +0300

    starters

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="container ">
+    <header>
+      <h1>Quizes</h1>
+      <input type="text" />
+    </header>
+    <div class="options-container"></div>
+  </div>
+</template>
+
+<style scoped></style>
