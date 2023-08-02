f1c5e36666da7bcf59cc4e6b2438b2a15c30d8ce
Kaleab Solomon <kaleabslmn@gmail.com>
2023-08-02 17:11:54 +0300

    dynamically render the questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,46 +1,24 @@
+<script setup>
+import { defineProps } from "vue";
+const { question } = defineProps(["question"]);
+</script>
 <template>
   <div class="mt-5">
-    <h1 class="text-3xl mb-5">question here</h1>
+    <h1 class="text-3xl mb-5">{{ question.text }}</h1>
   </div>
   <div class="options-container">
-    <div class="flex mb-5 cursor-pointer">
+    <div
+      v-for="option in question.options"
+      :key="option.id"
+      class="flex mb-5 cursor-pointer"
+    >
       <p
         class="bg-blue-400 w-10 h-10 text-3xl flex items-center justify-center"
       >
-        A
+        {{ option.label }}
       </p>
       <div class="bg-blue-300 w-full text-3xl px-5 py-0">
-        <p>answer</p>
-      </div>
-    </div>
-    <div class="flex mb-5 cursor-pointer">
-      <p
-        class="bg-blue-400 w-10 h-10 text-3xl flex items-center justify-center"
-      >
-        B
-      </p>
-      <div class="bg-blue-300 w-full text-3xl px-5 py-0">
-        <p>answer</p>
-      </div>
-    </div>
-    <div class="flex mb-5 cursor-pointer">
-      <p
-        class="bg-blue-400 w-10 h-10 text-3xl flex items-center justify-center"
-      >
-        C
-      </p>
-      <div class="bg-blue-300 w-full text-3xl px-5 py-0">
-        <p>answer</p>
-      </div>
-    </div>
-    <div class="flex mb-5 cursor-pointer">
-      <p
-        class="bg-blue-400 w-10 h-10 text-3xl flex items-center justify-center"
-      >
-        D
-      </p>
-      <div class="bg-blue-300 w-full text-3xl px-5 py-0">
-        <p>answer</p>
+        <p>{{ option.text }}</p>
       </div>
     </div>
   </div>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,13 +2,22 @@
 import { useRoute } from "vue-router";
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import { ref } from "vue";
+import quizes from "../assets/data/quizes.json";
+const route = useRoute();
+// get the id of the quiz
+const quizId = parseInt(route.params.id);
+
+const quiz = quizes.find((q) => q.id == quizId);
+
+const currentQuestionIndex = ref(0);
 </script>
 
 <template>
   <div>
     <QuizHeader />
     <div>
-      <Question />
+      <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
   </div>
 </template>
