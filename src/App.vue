25e91e1571c739fbb99a07e9de7ce7d6afd14f69
Kaleab Solomon <kaleabslmn@gmail.com>
2023-08-02 12:52:23 +0300

    add routing

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <script setup>
-import Card from "./components/Card.vue";
-import q from "./assets/data/quizes.json";
-import { ref, watch } from "vue";
-
-const quizes = ref(q);
-const search = ref("");
-// use watch to apply searching
-// watch accepts 2 parameters: the state being monitored and a callback function to be executed when the state changes.
-watch(search, () => {
-  quizes.value = q.filter((quiz) =>
-    quiz.name.toLowerCase().includes(search.value.toLowerCase())
-  );
-});
+import { RouterView } from "vue-router";
+import QuizsView from "./views/QuizesView.vue";
 </script>
 
 <template>
   <div class="max-w-5xl my-0 mx-auto">
-    <header class="mb-2.5 mt-7 flex items-center">
-      <h1 class="font-bold mr-7">Quizes</h1>
-      <input
-        v-model.trim="search"
-        class="border-0 bg-inputColor p-2.5 rounded-sm"
-        type="text"
-        placeholder="Search..."
-      />
-    </header>
-    <div class="flex flex-wrap mt-10">
-      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
-    </div>
+    <RouterView />
   </div>
 </template>
 
---
+++ src/views/QuizesView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import Card from "../components/Card.vue";
+import q from "../assets/data/quizes.json";
+import { ref, watch } from "vue";
+
+const quizes = ref(q);
+const search = ref("");
+// use watch to apply searching
+// watch accepts 2 parameters: the state being monitored and a callback function to be executed when the state changes.
+watch(search, () => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
+</script>
+
+<template>
+  <div>
+    <header class="mb-2.5 mt-7 flex items-center">
+      <h1 class="font-bold mr-7">Quizes</h1>
+      <input
+        v-model.trim="search"
+        class="border-0 bg-inputColor p-2.5 rounded-sm"
+        type="text"
+        placeholder="Search..."
+      />
+    </header>
+    <div class="flex flex-wrap mt-10">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
