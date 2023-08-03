4c3760d33e42e3c7231d35396ca93f9dc44290e1
Kaleab Solomon <kaleabslmn@gmail.com>
2023-08-03 15:56:31 +0300

    add progress

--- src/components/QuizHeader.vue
+++ src/components/QuizHeader.vue
@@ -1,8 +1,16 @@
+<script setup>
+import { defineProps } from "vue";
+
+const { questionStatus, barPercentage } = defineProps([
+  "questionStatus",
+  "barPercentage",
+]);
+</script>
 <template>
   <header class="mt-5">
-    <h4 class="text-3xl">question 1/3</h4>
+    <h4 class="text-3xl">Question {{ questionStatus }}</h4>
     <div class="w-[300px] h-[50px] border-blue-400 border-4">
-      <div class="h-full w-0 bg-blue-400"></div>
+      <div class="h-full bg-blue-400" :style="{ width: barPercentage }"></div>
     </div>
   </header>
 </template>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,23 +2,44 @@
 import { useRoute } from "vue-router";
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "../assets/data/quizes.json";
 const route = useRoute();
 // get the id of the quiz
 const quizId = parseInt(route.params.id);
 
-const quiz = quizes.find((q) => q.id == quizId);
+const quiz = quizes.find((q) => q.id === quizId);
 
 const currentQuestionIndex = ref(0);
+
+// const questionStatus = ref(
+//   `${currentQuestionIndex.value}/${quiz.questions.length}`
+// );
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+//   }
+// );
+
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+});
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
 </script>
 
 <template>
   <div>
-    <QuizHeader />
+    <QuizHeader
+      :barPercentage="barPercentage"
+      :questionStatus="questionStatus"
+    />
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
+    <button @click="currentQuestionIndex++">next Question</button>
   </div>
 </template>
 
